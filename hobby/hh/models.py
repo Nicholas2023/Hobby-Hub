d5d4eb6e2d905d42ccf3335ba3520d6733ffb3aa
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2024-03-04 13:42:24 +0300

    pycodestyle std

--- hobby/hh/models.py
+++ hobby/hh/models.py
@@ -8,8 +8,10 @@ from datetime import date
 
 # Create your models here.
 
+
 class User(AbstractUser):
     pass
+
 
 class UserProfile(models.Model):
     HOBBY_CHOICES = [
@@ -18,41 +20,52 @@ class UserProfile(models.Model):
         ('pottery', 'POTTERY'),
         ('bingewatching', 'BINGEWATCHING'),
     ]
-    
+
     SEEK_CHOICES = [
         ('casual relationship', 'CASUAL RELATIONSHIP'),
         ('romantic relationship', 'ROMANTIC RELATIONSHIP'),
         ('not sure', 'NOT SURE'),
     ]
-    
-    seeking = MultiSelectField(choices=SEEK_CHOICES, default=('romantic relationship',), validators=[MaxValueValidator(3)])
-    username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_profiles')
+
+    seeking = MultiSelectField(choices=SEEK_CHOICES,
+                               default=('romantic relationship',),
+                               validators=[MaxValueValidator(3)])
+    username = models.ForeignKey(User, on_delete=models.CASCADE,
+                                 related_name='user_profiles')
     location = models.CharField(max_length=99, null=True)
-    hobbies = MultiSelectField(choices=HOBBY_CHOICES, default=('reading',), validators=[MaxValueValidator(3)])
+    hobbies = MultiSelectField(choices=HOBBY_CHOICES,
+                               default=('reading',),
+                               validators=[MaxValueValidator(3)])
     gender = models.CharField(max_length=65, default='female')
     bio = models.CharField(max_length=300)
     age = models.IntegerField(null=False)
-    
+
     class Meta:
         db_table = 'UserProfile'
-        
+
     def calculate_age(self, born):
         today = date.today()
-        return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+        return today.year - born.year - (
+                        (today.month, today.day) < (born.month, born.day))
+
 
 class ProfileForm(ModelForm):
     class Meta:
         model = UserProfile
         fields = ['location', 'hobbies', 'gender', 'age']
-        
+
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.user
 
+
 class Message(models.Model):
-    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="messages")
-    sender = models.ForeignKey("User", on_delete=models.PROTECT, related_name="messages_sent")
-    recipients = models.ManyToManyField("User", related_name="messages_received")
+    user = models.ForeignKey("User", on_delete=models.CASCADE,
+                             related_name="messages")
+    sender = models.ForeignKey("User", on_delete=models.PROTECT,
+                               related_name="messages_sent")
+    recipients = models.ManyToManyField("User",
+                                        related_name="messages_received")
     body = models.TextField(blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     read = models.BooleanField(default=False)
